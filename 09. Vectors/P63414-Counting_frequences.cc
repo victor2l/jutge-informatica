fb9b1e8d976304f820eda63f796b1c1c2f9f4044
victor2l <94573776+victor2l@users.noreply.github.com>
2021-11-17 23:38:42 +0100

    Update P63414-Counting_frequences.cc
    Aquesta versió és més sencilla.

--- 09. Vectors/P63414-Counting_frequences.cc
+++ 09. Vectors/P63414-Counting_frequences.cc
@@ -2,55 +2,16 @@
 #include <vector>
 using namespace std;
 
-
 int main() {
-	int n;
-	int nombre;
-	cin >> n;
-	vector<int> llista(n);
-	vector<int> diferents;
-	int nombre_diferents = diferents.size();
-	vector<int> ordenats;
-	for (int l = 0; l < n; l++) {
-		cin >> nombre;
-		nombre = nombre%1000000000;
-		llista[l] = nombre;
-	}
-	for (int i = 0; i < n; i++) {
-		bool nou_vector = false;
-		for (int j = 0; j < nombre_diferents; j++) {
-			if (llista[i] == diferents[j]) nou_vector = true;
-		}
-		if (nou_vector) {
-				bool para = false;
-				int j = 0;
-				while (j < nombre_diferents and not para) {
-					if (diferents[j] > llista[i]) {
-						para = true;
-						for (int l = nombre_diferents; l > j+1; l--) {
-							if (l != nombre_diferents) {
-								diferents.push_back(diferents[nombre_diferents]);
-							}
-							else {
-								diferents[l] = diferents[l-1];
-							}				
-						}
-						diferents[j+1] = llista[i];
-					}
-				}					 
-			if (not para) diferents.push_back(llista[i]);
-			nombre_diferents++;
-		}
-		else if (i == 1) diferents.push_back(llista[i]);
-	}
-	vector<int> comptador(nombre_diferents);
-	for (int j = 0; j < nombre_diferents; j++) {
-		for (int i = 0; i < n; i++) {
-			if (llista[i] == diferents[j]) comptador[j]++;
-		}
-	}
-	
-	for (int i = 0; i < nombre_diferents; i++) {
-		cout << 1000000 << diferents[i] << " : " << comptador[i] << endl;
-	}
+  int n, x; // n = nombre d'elements. x = cada element.
+  cin >> n;
+  vector<int> v(1001, 0); // Vector v de 1001 elements inicialitzats a zero.
+  for (int i = 0; i < n; ++i) {
+    cin >> x;
+    x = x - 1000000000;
+    v[x] = 1 + v[x];
+  }
+  for (int i = 0; i < 1001; ++i)
+    if (v[i] > 0)
+      cout << 1000000000 + i << " : " << v[i] << endl;
 }
