c0130895d0f95a8400602e2698ed7e823248a3b9
victor2l <94573776+victor2l@users.noreply.github.com>
2021-11-25 00:51:34 +0100

    Update P72484-Rhombus.cc
    Aquesta és una versió més clara i senzilla, amb loops de tipus "for" en comptes de "while".

--- 03. Loops inside loops/P72484-Rhombus.cc
+++ 03. Loops inside loops/P72484-Rhombus.cc
@@ -2,41 +2,20 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    int aux = n;
-    int aux2 = 2*n -1;
-    int z = n;
-    int j = 1;
-    while (j < z) {  //escriu el triangle superior
-        int i = 1;
-        while (i < aux) {
-            cout << " ";
-            i = i+1;
-        }
-        int k = 0;
-        while (k < 2*j - 1) {
-            cout << "*";
-            k = k+1;
-        }
-        cout << endl;
-        j = j+1;
-        aux = aux - 1;
-    }
-    int c = 0;
-    while (c < z) {
-		int i = 0;
-		while (i < c) {
-			cout << " ";
-			i = i+1;
-		}
-		int k = 0;
-		while (k < aux2) {
-			cout << "*";
-			k = k+1;
-		}
-		cout << endl;
-		c = c+1;
-		aux2 = aux2 - 2;
-	}
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; ++i) { // escriu les n primeres files
+    for (int j = n - i; j > 0; --j)
+      cout << ' ';
+    for (int k = 1; k < 2 * i; ++k)
+      cout << '*';
+    cout << endl;
+  }
+  for (int i = 1; i < n; ++i) { // escriu les n-1 files restants
+    for (int j = 0; j < i; ++j)
+      cout << ' ';
+    for (int k = 2 * n - 1; k > 2 * i; --k)
+      cout << '*';
+    cout << endl;
+  }
 }
